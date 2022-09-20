fdc168a61b4c1738968c4e197a3be73f5fd4fe63
Shevshelev Lev <shevshelev.lev@gmail.com>
2022-09-20 13:37:08 +0500

    Initial Commit

---
+++ The Rick and Morty Aston/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Rick and Morty Aston
+//
+//  Created by Shevshelev Lev on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
